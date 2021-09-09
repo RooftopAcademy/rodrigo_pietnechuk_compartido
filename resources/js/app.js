c5329b75c40623c62d4d1e4a26c61fb8d4e86f02
ghnoob <ptnchk.rodrigo@gmail.com>
2021-09-09 15:27:37 -0300

    feat: adapted for bigger resolutions

--- resources/js/app.js
+++
@@ -1,7 +0,0 @@
-const burguer = document.querySelector('#burguer-button');
-const menu = document.querySelector('.menu');
-
-burguer.addEventListener('click', () => {
-	menu.classList.toggle('menu-show');
-  menu.classList.toggle('menu-hide');
-});
