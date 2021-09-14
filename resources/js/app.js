06513ad004114011dbedfdcc660b4682163e84ff
ghnoob <ptnchk.rodrigo@gmail.com>
2021-09-14 01:02:02 -0300

    refactor: change top bar for sidebar

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+const burguerMenu = document.querySelector(".burger-menu");
+
+burguerMenu.addEventListener("click", () => {
+  const navigation = document.querySelector(".navigation");
+  navigation.classList.toggle("hidden");
+});
