e8a5032b37d97e9b84b78ef40ebf210731db124a
ghnoob <ptnchk.rodrigo@gmail.com>
2021-09-14 13:32:21 -0300

    fix: delete repeated css

--- resources/js/product-list.js
+++ resources/js/product-list.js
@@ -3,7 +3,7 @@ const products = document.querySelector(".products");
 for (let i = 0; i < 12; i++) {
   products.innerHTML += `
     <article class="products-item">
-      <a class="link d-flex text-left" href="product-details.html">
+      <a class="link d-flex justify-align-center text-left" href="product-details.html">
         <img class="image product-image" src="resources/assets/white-tshirt.png">
         <div class="caption">
           <p>Remera Blanca Algodón</p>
