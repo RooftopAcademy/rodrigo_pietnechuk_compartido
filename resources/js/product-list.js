aea836a1b2ebac96e73eb42c83e125140c1d2910
ghnoob <ptnchk.rodrigo@gmail.com>
2021-09-14 01:06:11 -0300

    refactor: css modularization

--- resources/js/product-list.js
+++ resources/js/product-list.js
@@ -4,7 +4,7 @@ for (let i = 0; i < 12; i++) {
   products.innerHTML += `
     <article class="products-item">
       <a class="link d-flex text-left" href="product-details.html">
-        <img class="product-image" src="resources/assets/white-tshirt.png">
+        <img class="image product-image" src="resources/assets/white-tshirt.png">
         <div class="caption">
           <p>Remera Blanca Algodón</p>
           <p><strong>$399,99</strong></p>
