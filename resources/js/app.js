7a356bf9bfebae0b8ac5b168e02252b0ab8a4b70
ghnoob <ptnchk.rodrigo@gmail.com>
2021-09-22 19:47:25 -0300

    add search function to search bar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,23 @@
-const header = document.querySelector(".header");
+const header = document.querySelector("header");
 const navigation = header.querySelector(".navigation");
+const suggestions = navigation.querySelector("#suggestions");
 
 header.querySelector(".burger-menu").addEventListener("click", () => {
   navigation.classList.toggle("hidden");
+});
+
+navigation.querySelector("#search-bar").addEventListener("input", async function() {
+  const value = this.value.trim();
+
+  if (value === "") {
+    suggestions.innerHTML = "";
+    return;
+  }
+
+  const res = await StoreApi.filterCatalog(value);
+  const data = await res.json();
+
+  renderSuggestions(data, suggestions);
 });
 
 const store = new Store();
