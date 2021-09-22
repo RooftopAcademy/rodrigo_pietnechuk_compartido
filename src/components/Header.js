3943407f0616ae22ad510cc9fe4541ba50c1379b
ghnoob <ptnchk.rodrigo@gmail.com>
2021-09-21 21:29:00 -0300

    create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+    <header>
+      <nav class="hidden d-flex flex-column text-left navigation">
+        <a class="link" href="index.html">
+          <i class="fas fa-store-alt"></i> Inicio
+        </a>
+        <a class="link" href="product-list.html">
+          <i class="fas fa-list-ul"></i> Lista de productos
+        </a>
+        <a class="link" href="#">
+          <i class="fas fa-search"></i> Buscar
+        </a>
+        <a class="link" href="#">
+          <i class="fas fa-shopping-cart"></i> Carrito
+        </a>
+        <a class="link" href="signup.html">
+          <i class="fas fa-user"></i> Iniciar sesión
+        </a>
+      </nav>
+      <button class="burger-menu">
+        <i class="fas fa-bars"></i>
+      </button>
+    </header>
+    `;
+  }
+}
+
+customElements.define('header-component', Header);
