eee6c24f4f19372ef56b406a8f8550d23bfe4c76
ghnoob <ptnchk.rodrigo@gmail.com>
2021-09-26 00:10:26 -0300

    fix signup form not working properly
    * fix form not rendering
    * fix form not validating input data

--- src/app/controllers/signup.ts
+++ src/app/controllers/signup.ts
@@ -1,11 +1,7 @@
 import getHTMLElement from "../helpers/getHTMLElement";
 
-export default function loadSignupForm(): void {
+export default function signupValidators(): void {
   const signupForm: HTMLFormElement = getHTMLElement("#signup-form") as HTMLFormElement;
-
-  signupForm.addEventListener("submit", (e: Event) => {
-    e.preventDefault();
-  });
 
   const password: HTMLInputElement = getHTMLElement(
     "input[name=password]",
@@ -13,21 +9,19 @@ export default function loadSignupForm(): void {
   ) as HTMLInputElement;
 
   const confirmPassword: HTMLInputElement = getHTMLElement(
-    "input[name=password]",
+    "input[name=confirmpassword]",
     signupForm,
   ) as HTMLInputElement;
 
-  password.addEventListener("input", function (): void {
-    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,30}$/;
-    const validityMessage = regex.test(this.value) ? ""
-      : "La contraseña debe contener entre 8 y 30 caracteres, y por lo menos una minúscula"
-      + " una mayúscula y un número";
-
-    this.setCustomValidity(validityMessage);
+  password.addEventListener("input", function (this: HTMLInputElement) {
+    this.setCustomValidity(
+      this.validity.patternMismatch
+        ? "Debe tener entre 8 y 30 caracteres, minúsculas, mayúsculas y números" : "",
+    );
   });
 
   confirmPassword.addEventListener("input", function (): void {
-    const validityMessage = signupForm.password.value == this.value ? ""
+    const validityMessage = password.value == this.value ? ""
       : "Las contraseñas no son iguales";
     this.setCustomValidity(validityMessage);
   });
--- src/app/views/signup.ts
+++ src/app/views/signup.ts
@@ -1,13 +1,20 @@
 import "../../public/resources/css/signup.css";
 
-export default function signup(): string {
-  return `
+export default function signup(el: HTMLElement): void {
+  el.innerHTML = `
   <form id="signup-form" class="d-flex flex-column">
     <input class="form-input" name="last-name" type="text" placeholder="Apellido" required>
     <input class="form-input" name="first-name" type="text" placeholder="Nombre" required>
     <input class="form-input" name="username" type="text" placeholder="Usuario" required>
     <input class="form-input" name="email" type="email" placeholder="Correo electrónico" required>
-    <input class="form-input" name="password" type="password" placeholder="Contraseña" required>
+    <input
+      class="form-input"
+      name="password"
+      type="password"
+      placeholder="Contraseña"
+      pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,30}$"
+      required
+    >
     <input
       class="form-input"
       name="confirmpassword"
