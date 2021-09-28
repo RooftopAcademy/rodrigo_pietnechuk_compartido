8f7b55e80a77ccd161fd3aa4955b18828d89473d
ghnoob <ptnchk.rodrigo@gmail.com>
2021-09-27 22:20:50 -0300

    add hash routes to app

--- src/app/models/App.ts
+++ src/app/models/App.ts
@@ -11,41 +11,21 @@ export default class App {
 
   public constructor(el: HTMLElement) {
     this.el = el;
-    this.navigate("/");
+    this.navigate("");
     this.store = new Store();
     this.store.fetchCatalog();
-    this.addRouterLinks("header .js-router-link");
+    window.addEventListener("hashchange", () => this.navigate(window.location.hash.split("/")[0]));
   }
 
   public navigate(route: string): void {
     const routes: Record<string, (el: HTMLElement) => void> = {
-      "/": home,
-      "/product-list": () => {
-        productList(this.el, this.store.catalog);
-        this.addRouterLinks(".products-item .js-router-link");
-      },
-      "/product-details": productDetails,
-      "/signup": signup,
+      "": home,
+      "#product-list": () => productList(this.el, this.store.catalog),
+      "#product-details": productDetails,
+      "#signup": signup,
     };
 
-    const selectedRoute = routes[route];
-    if (selectedRoute) {
-      selectedRoute(this.el);
-    } else {
-      notFound(this.el);
-    }
-  }
-
-  public addRouterLinks(selector: string): void {
-    Array.from(document.querySelectorAll(selector)).forEach((item: Element) => {
-      const link = item as HTMLLinkElement;
-      link.addEventListener("click", (e: MouseEvent): void => {
-        e.preventDefault();
-        const target: HTMLLinkElement = e.currentTarget as HTMLLinkElement;
-        const url: URL = new URL(target.href);
-        sessionStorage.setItem("id", url.searchParams.get("id") || "");
-        this.navigate(url.pathname);
-      });
-    });
+    const selectedRoute = routes[route] || notFound;
+    selectedRoute(this.el);
   }
 }
