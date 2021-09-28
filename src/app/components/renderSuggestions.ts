c683a414631b999e8ad82479e61788ae346e943f
ghnoob <ptnchk.rodrigo@gmail.com>
2021-09-27 22:22:18 -0300

    add hash routes to search

--- src/app/components/renderSuggestions.ts
+++ src/app/components/renderSuggestions.ts
@@ -1,15 +1,13 @@
 import IBook from "../interfaces/IBook";
-import app from "../main";
 
 export default function renderSuggestions(data: IBook[], domElement: HTMLUListElement): void {
   const option = (item: IBook): string => `
     <li class="option">
-      <a class="link js-router-link max-width" href="/product-details?id=${item.id}">
+      <a class="link max-width" href="#product-details/${item.id}">
         ${item.name}
       </a>
     </li>
   `;
 
   domElement.innerHTML = data.reduce((acc: string, cur: IBook) => acc + option(cur), "");
-  app.addRouterLinks(".option .js-router-link");
 }
