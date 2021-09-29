361ad3d36aa3d231dcf3498886099cb04094e681
ghnoob <ptnchk.rodrigo@gmail.com>
2021-09-29 16:05:50 -0300

    add functionality to login form

--- src/app/views/renderLogin.ts
+++ src/app/views/renderLogin.ts
@@ -1,10 +1,27 @@
 import '../../public/resources/css/login.css';
 import getHTMLElement from '../helpers/getHTMLElement';
+import getUserIdFromApi from '../helpers/getUserIdFromApi';
+import login from '../helpers/login';
+import logout from '../helpers/logout';
+import type IApiUser from '../interfaces/IApiUser';
 
 function addEvents(el: HTMLElement): void {
   const loginForm: HTMLFormElement = getHTMLElement('#login-form', el) as HTMLFormElement;
-  loginForm.addEventListener('submit', (e: Event) => {
+  const output: HTMLOutputElement = getHTMLElement('#output', el) as HTMLOutputElement;
+
+  logout();
+
+  loginForm.addEventListener('submit', async function (this: HTMLFormElement, e: Event) {
     e.preventDefault();
+    try {
+      output.value = '';
+      const user: IApiUser = await getUserIdFromApi(this.username.value, this.email.value);
+      login(user);
+      window.location.hash = '';
+    } catch (error) {
+      const err = error as Error;
+      output.value = `Error: ${err.message}`;
+    }
   });
 }
 
@@ -15,6 +32,7 @@ export default function renderLogin(el: HTMLElement): void {
     <input class="form-input" name="email" type="email" placeholder="Correo electrónico" required>
     <button class="button" type="submit">Iniciar sesión</button>
   </form>
+  <output id="output" class="text-red">
   `;
 
   addEvents(el);
