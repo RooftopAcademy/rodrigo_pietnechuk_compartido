7a074d4271ff3a0d5814b34989fa913a3d9434ee
ghnoob <ptnchk.rodrigo@gmail.com>
2021-09-30 15:29:21 -0300

    fix: linting errors

--- src/app/helpers/getUserIdFromApi.ts
+++ src/app/helpers/getUserIdFromApi.ts
@@ -3,7 +3,10 @@ import UserApi from '../services/UserApi';
 import makeRequest from '../services/makeRequest';
 
 async function getUserLoginFromApi(username: string, email: string): Promise<ApiUserInterface> {
-  const users: ApiUserInterface[] = await makeRequest(UserApi.getByUsernameAndEmail(username, email));
+  const users: ApiUserInterface[] = await makeRequest(
+    UserApi.getByUsernameAndEmail(username, email),
+  );
+
   const user: ApiUserInterface = users[0];
   if (!user) {
     throw new Error('Usuario o contraseña incorrectos.');
