ea478e86763e152bbdfadf301a427b5ac4ce6369
ghnoob <ptnchk.rodrigo@gmail.com>
2021-10-01 13:27:51 -0300

    refactor: extract function to render item in product list

---
+++ src/app/components/getListItemInnerHTML.ts
@@ -0,0 +1,16 @@
+import type Book from '../entities/Book';
+
+export default function listItem(item: Book): string {
+  return `
+    <article class="products-item">
+      <a class="link d-flex text-left" href="#product-details/${item.id}">
+        <img class="image product-image" src="${item.imageURL}">
+        <div class="caption">
+          <p class="larger-text"><em>${item.name}</em></p>
+          <p>${item.author}</p>
+          <p><strong>$${item.price}</strong></p>
+        </div>
+      </a>
+    </article>
+  `;
+}
--- src/app/views/ProductList.ts
+++ src/app/views/ProductList.ts
@@ -1,4 +1,5 @@
 import type Book from '../entities/Book';
+import getListItemInnerHTML from '../components/getListItemInnerHTML';
 import View from '../abstract/View';
 import Catalog from '../collections/Catalog';
 
@@ -6,25 +7,12 @@ import '../../public/resources/css/product-list.css';
 
 export default class ProductList extends View {
   public async render(): Promise<void> {
-    const listItem = (item: Book): string => `
-      <article class="products-item">
-        <a class="link d-flex text-left" href="#product-details/${item.id}">
-          <img class="image product-image" src="${item.imageURL}">
-          <div class="caption">
-            <p class="larger-text"><em>${item.name}</em></p>
-            <p>${item.author}</p>
-            <p><strong>$${item.price}</strong></p>
-          </div>
-        </a>
-      </article>
-    `;
-
     const catalog = new Catalog();
     await catalog.fetchCatalog();
 
     this.el.innerHTML = `
       <div class="max-width d-flex flex-column space-between small-text products">
-        ${catalog.products.reduce((a: string, b: Book): string => a + listItem(b), '')}
+        ${catalog.products.reduce((a: string, b: Book): string => a + getListItemInnerHTML(b), '')}
       </div>
     `;
   }
