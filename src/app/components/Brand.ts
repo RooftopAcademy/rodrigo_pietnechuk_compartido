0ff3f9e933211c45ad5a9c431223f434045896cf
ghnoob <ptnchk.rodrigo@gmail.com>
2021-10-12 00:23:05 -0300

    fix: center logo

--- src/app/components/Brand.ts
+++ src/app/components/Brand.ts
@@ -1,7 +1,7 @@
 export default class Brand extends HTMLElement {
   private connectedCallback() {
     this.innerHTML = `
-    <section class="brand">
+    <section class="text-center">
       <i class="fas fa-store-alt logo"></i>
       <h1>EMPRESA S.A.</h1>
       <p><em>Una empresa que vende libros</em></p>
