2983e8939406f2eac12af37c7b4ce1b6aeb3251d
ghnoob <ptnchk.rodrigo@gmail.com>
2021-10-14 20:19:19 -0300

    refactor: webpack: separate file types in folders

--- webpack.config.js
+++ webpack.config.js
@@ -22,13 +22,27 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.svg$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'static/svg/[hash][ext][query]',
+        },
+      },
+      {
+        test: /\.(ttf|eot|woff2?)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'static/fonts/[hash][ext][query]',
+        },
+      },
     ],
   },
   resolve: {
     extensions: ['.ts', '.js'],
   },
   output: {
-    filename: 'bundle.js',
+    filename: 'static/js/bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
