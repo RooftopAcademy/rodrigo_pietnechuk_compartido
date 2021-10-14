ba62d23cd7c2d6b3645e757f76102424d58fda27
ghnoob <ptnchk.rodrigo@gmail.com>
2021-10-14 20:19:49 -0300

    refactor: change static and home routes

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,17 @@
+import path from 'path';
 import { log } from 'debug';
 import dotenv from 'dotenv';
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 dotenv.config();
 
 const app: express.Application = express();
 
-app.use(express.static('dist'));
+app.use('/static', express.static(path.resolve('dist', 'static')));
+
+app.get('/', (_req: Request, res: Response): void => {
+  res.sendFile(path.resolve('dist', 'index.html'));
+});
 
 const port: string = process.env.PORT || '3000';
 app.listen(port, () => {
