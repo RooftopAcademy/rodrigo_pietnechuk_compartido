a11187cea4e6f8f29e796ce76e942495619c5830
ghnoob <ptnchk.rodrigo@gmail.com>
2021-10-14 11:45:06 -0300

    add landing page

--- src/app/views/Home.ts
+++ src/app/views/Home.ts
@@ -1,29 +1,25 @@
 import '../../public/resources/css/index.css';
 import View from '../abstract/View';
+import Catalog from '../collections/Catalog';
+import getRecommendedProductInnerHTML from '../components/getRecommendedProductInnerHTML';
 
 export default class Home extends View {
-  public render(): void {
+  public async render(): Promise<void> {
+    const catalog: Catalog = new Catalog();
+    await catalog.fetch();
+
     this.el.innerHTML = `
-    <section class="about">
-      <h2>Sobre nosotros</h2>
-      <article class="text-justify">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque veritatis laudantium modi
-          provident? Fuga, nihil cupiditate ullam officia quod aut sint esse distinctio quidem
-          voluptas ab at atque deserunt doloremque!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque veritatis laudantium modi
-          provident? Fuga, nihil cupiditate ullam officia quod aut sint esse distinctio quidem
-          voluptas ab at atque deserunt doloremque!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque veritatis laudantium modi
-          provident? Fuga, nihil cupiditate ullam officia quod aut sint esse distinctio quidem
-          voluptas ab at atque deserunt doloremque!
-        </p>
-      </article>
-    </section>
+      <div class="landing">
+        <img
+          class="banner"
+          src="https://s3.amazonaws.com/images-cdn.libooks.com/LBS_004_Ahora3y6_v1_desktop_8d05b503f2.jpg"
+          alt="banner"
+        >
+        <h1 class="text-left">Recomendado para tí</h1>
+        <div>
+          ${getRecommendedProductInnerHTML(catalog.getRandomItem())}
+        </div>
+      </div>
     `;
     super.render();
   }
