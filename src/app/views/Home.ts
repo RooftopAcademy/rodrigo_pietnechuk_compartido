96f8e0cbc6ce2b73bda0fea10ce1ae17f1b4020c
ghnoob <ptnchk.rodrigo@gmail.com>
2021-10-14 13:04:49 -0300

    add shipment info

--- src/app/views/Home.ts
+++ src/app/views/Home.ts
@@ -15,6 +15,20 @@ export default class Home extends View {
           src="https://s3.amazonaws.com/images-cdn.libooks.com/LBS_004_Ahora3y6_v1_desktop_8d05b503f2.jpg"
           alt="banner"
         >
+        <div class="shipment mt-2 mx-1">
+          <h3>Envíos</h3>
+          <div class="shipment-grid small-text">
+            <i class="fa fa-map-marker-alt icon"></i>
+            <i class="fa fa-truck icon"></i>
+            <i class="fa fa-shipping-fast icon"></i>
+            <h4 class="m-0 p-0">A todo el país</h4>
+            <h4 class="m-0 p-0">Gratis</h4>
+            <h4 class="m-0 p-0">Express</h4>
+            <p class="m-0">Recibí tu libro donde quieras</p>
+            <p class="m-0">En compras de más de $3500</p>
+            <p class="m-0">Para AMBA</p>
+          </div>
+        </div>
         <h1 class="text-left">Recomendado para tí</h1>
         <div>
           ${getRecommendedProductInnerHTML(catalog.getRandomItem())}
