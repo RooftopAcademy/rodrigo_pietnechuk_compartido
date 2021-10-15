527c4a882c1e86c550365c8d907e5816b9143c33
ghnoob <ptnchk.rodrigo@gmail.com>
2021-10-15 14:08:25 -0300

    connect client to new server
    old servers conserved as fallback

--- src/client/services/StoreApi.ts
+++ src/client/services/StoreApi.ts
@@ -1,12 +1,14 @@
 export default class StoreApi {
-  private static readonly baseURL = 'https://my-json-server.typicode.com/ghnoob/rooftop-mock-db';
+  private static readonly fallback: string = 'https://my-json-server.typicode.com/ghnoob/rooftop-mock-db';
+  private static readonly baseURL: string = process.env.API_URL || this.fallback;
 
   public static getCatalog(): Promise<Response> {
     return fetch(`${this.baseURL}/catalog`);
   }
 
+  // TODO: add filetering to api
   public static filterCatalog(filter: string): Promise<Response> {
-    return fetch(`${this.baseURL}/catalog?name_like=${filter}&_limit=10`);
+    return fetch(`${this.fallback}/catalog?name_like=${filter}&_limit=10`);
   }
 
   public static getBookById(id: string): Promise<Response> {
--- src/client/services/UserApi.ts
+++ src/client/services/UserApi.ts
@@ -1,11 +1,12 @@
 export default class UserApi {
-  private static readonly baseURL: string = 'https://jsonplaceholder.typicode.com/users';
+  private static readonly fallback: string = 'https://jsonplaceholder.typicode.com';
+  private static readonly baseURL: string = process.env.API_URL || this.fallback;
 
   public static getById(id: string): Promise<Response> {
-    return fetch(`${this.baseURL}/${id}`);
+    return fetch(`${this.baseURL}/users/${id}`);
   }
 
   public static getByUsernameAndEmail(username: string, email: string): Promise<Response> {
-    return fetch(`${this.baseURL}?username=${username}&email=${email}`);
+    return fetch(`/users/${this.baseURL}?username=${username}&email=${email}`);
   }
 }
